63eb8b1eb25783bb2289a321b02d57c31f2acaf0
mwilhelm85 <Matt.Wilhelm@web.de>
2012-11-20 15:33:05 +0100

    added a line

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("This is a very simple \"Hello World\" program");
+		
+		System.out.println("This is just done for a first try with Git");
 	}
 
 }

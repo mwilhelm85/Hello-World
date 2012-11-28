655365eb943244c5629a9b269d8a7843b857e1ee
mwilhelm85 <Matt.Wilhelm@web.de>
2012-11-28 09:48:55 +0100

    asdf

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,8 +7,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("This is a very simple \"Hello World\" program");
 		
-		System.out.println("This is just done for a first try with Git222");
-		System.out.println("just a line for a new test333");
+		System.out.println("This is just done for a first try with Git");
 	}
 
 }

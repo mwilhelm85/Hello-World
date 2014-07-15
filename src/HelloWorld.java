44ad18c6a5832298d98fbf7580fb94e5cb0e3d93
MWilhelm85 <Matt.Wilhelm@web.de>
2014-07-15 15:29:03 +0200

    its for both, first and second try

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("This is a very simple \"Hello World\" program");
 		
-		System.out.println("This is just done for a first try with Git");
+		System.out.println("This is just done for a first (and second) try with Git");
 	}
 
 }
